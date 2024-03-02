3f6a9ea79def5fae0368da0de5740ef8e26b741f
Esqulapa <jaroslaw.zegzula@gmail.com>
2024-03-02 15:12:13 +0100

    initial commit

---
+++ src/main/java/pl/jaroslaw/zegzula/flashcard/UploadFileController.java
@@ -0,0 +1,14 @@
+package pl.jaroslaw.zegzula.flashcard;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("/api/v1/uploader")
+public class UploadFileController {
+
+   
+
+   
+
+
+}
---
+++ src/test/java/pl/jaroslaw/zegzula/flashcard/FlashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jaroslaw.zegzula.flashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
